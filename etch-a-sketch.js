5cca3ecf93f87e07bf9e74d5cf8a804047e8f752
bstephe9 <bstephenson9807@gmail.com>
2025-03-17 20:56:18 -0400

    feat: Change square color on mouse hover

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,11 +3,16 @@ const grid = document.querySelector(".grid")
 const rows = 16
 const cols = 16
 
+const onMouseHover = (e) => {
+  e.target.style.backgroundColor = "blue"
+}
+
 // Create a grid of square divs
 for (let i = 0; i < rows; i++) {
   for (let j = 0; j < cols; j++) {
     const square = document.createElement("div")
     square.classList.add("square")
+    square.addEventListener("mouseover", onMouseHover)
     grid.appendChild(square)
   }
 }
